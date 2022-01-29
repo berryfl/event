83dfa38ce6f2d53d27136b30111cee679c40f5b1
李文俊 <berryfl@gmail.com>
2022-01-29 18:59:29 +0800

    add create method to Instance

--- model/po/instance.go
+++ model/po/instance.go
@@ -1,6 +1,8 @@
 package po
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -8,4 +10,12 @@ type Instance struct {
 	gorm.Model
 	UUID  string `gorm:"uuid"`
 	Stage string `gorm:"stage"`
+}
+
+func (inst *Instance) Create(db *gorm.DB) error {
+	result := db.Create(inst)
+	if result.Error != nil {
+		return fmt.Errorf("create instance failed: %w", result.Error)
+	}
+	return nil
 }
